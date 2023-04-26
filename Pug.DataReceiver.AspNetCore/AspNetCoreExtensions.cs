544380a761045dea9e22b12473dc5ce59057c572
Andrian <Andrian@ND.id.au>
2023-04-26 23:12:28 +1000

    Endpoint route registration enhancements.

--- Pug.DataReceiver.AspNetCore/AspNetCoreExtensions.cs
+++ Pug.DataReceiver.AspNetCore/AspNetCoreExtensions.cs
@@ -54,10 +54,11 @@ namespace Pug.DataReceiver.Http.Components
 		}
 
 		/// <summary>
-		/// 
+		/// Register data receiver with specified <paramref name="basePath"/>.
 		/// </summary>
 		/// <param name="webApplication">Instance of <c>WebApplication</c></param>
 		/// <param name="basePath">Base path on which data receiver endpoint should be registered and may be more one or more levels relative to root path</param>
+		/// <param name="allowSubPath">Whether data may be submitted to sub-path under <paramref name="basePath"/></param>
 		/// <param name="authenticationHeader">Name of request header from which credentials should be retrieved</param>
 		/// <param name="credentialsParser">Optional credentials parser</param>
 		/// <param name="resultHandler">Optional <c>IDataReceiver.SubmitAsync</c> result to <c>IResult</c> mapping.</param>
@@ -65,29 +66,39 @@ namespace Pug.DataReceiver.Http.Components
 		/// <returns></returns>
 		public static RouteHandlerBuilder UseDataReceiver<T>(
 			this WebApplication webApplication,
-			string basePath, string authenticationHeader,
+			string basePath = "/", bool allowSubPath = false, string? authenticationHeader = null,
 			Func<string, Task<IDictionary<string, string>>>? credentialsParser = null,
 			Func<OneOf<Unit, Exception>, Task<IResult>>? resultHandler = null )
 			where T : IDataReceiver
 		{
-			async Task<IResult> ReceiveData( string path, HttpContext context )
+			async Task<IResult> ReceiveData( string? path, HttpContext context )
 			{
+				if( !allowSubPath && !string.IsNullOrWhiteSpace( path ) )
+					return Results.NotFound();
+				
 				if( context.Request.ContentLength == 0 ) return Results.BadRequest();
 
 				T? dataReceiver = context.RequestServices.GetService<T>();
 
 				if( dataReceiver is null ) return Results.Problem( new ProblemDetails() { Status = 500 } );
 
-				IDictionary<string, string>? credentials = await GetCredentialsAsync( context, authenticationHeader, credentialsParser );
+				IDictionary<string, string>? credentials = string.IsNullOrWhiteSpace(authenticationHeader)?
+																null :
+																await GetCredentialsAsync( context, authenticationHeader, credentialsParser );
 
-				OneOf<Unit, Exception> result = await dataReceiver?.SubmitAsync( credentials, path, context.Request.Body, context.Request.ContentType )!;
+				OneOf<Unit, Exception> result = await dataReceiver.SubmitAsync( credentials, path, context.Request.Body, context.Request.ContentType )!;
 
 				if( resultHandler is null ) return Handle( result );
 
 				return await resultHandler( result );
 			}
 
-			return webApplication.MapPost( $"/{basePath}/{{**path}}", ReceiveData );
+			string route = basePath.TrimEnd( '/' );
+			route = route.StartsWith('/')? route : $"/{route}";
+
+			route = $"{route}/{{**path?}}";
+			
+			return webApplication.MapPost( route, ReceiveData );
 		}
 	}
 }
